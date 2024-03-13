ca9fc2864407c2d2fe565dedf610121ec80d62f4
Волкова Виктория <vvolkova@diasoft.ru>
2024-03-13 10:49:12 +0300

    exercise-3
    Перенос из exercise-3 https://gitflex.diasoft.ru/vvolkova_spring/otus-spring/-/tree/exercise-3-1?ref_type=heads

---
+++ src/main/java/ru/diasoft/otusspring/service/QuestionService.java
@@ -0,0 +1,12 @@
+package ru.diasoft.otusspring.service;
+
+import com.opencsv.exceptions.CsvValidationException;
+import ru.diasoft.otusspring.domain.Question;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public interface QuestionService {
+    void testingStudent() throws CsvValidationException, IOException;
+}
