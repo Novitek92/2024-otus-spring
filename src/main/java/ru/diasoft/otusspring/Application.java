96b44e06469f66979616eb6a6290b51cc8199ac7
Волкова Виктория <vvolkova@diasoft.ru>
2024-04-05 14:21:24 +0300

    exercise-7

--- src/main/java/ru/diasoft/otusspring/Application.java
+++ src/main/java/ru/diasoft/otusspring/Application.java
@@ -2,16 +2,12 @@ package ru.diasoft.otusspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.diasoft.otusspring.service.QuestionService;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		QuestionService service = context.getBean(QuestionService.class);
-		service.resultTestingStudent(service.testingStudent());
+		SpringApplication.run(Application.class, args);
 	}
 
 }
